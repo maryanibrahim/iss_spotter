6521a81281b60d5b4ddb3486354ad5bb95cf86f0
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-19 12:17:43 +0000

    index2

---
+++ index2.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+const { printPassTimes } = require('./index');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
